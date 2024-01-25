342e69785e39f3925fcc20b0f41b02d23d6b45a3
Rahul Basu <142247733+r-basu@users.noreply.github.com>
2024-01-24 19:34:53 -0500

    cleanup code slightly and add temp/speed/humdity to currentWeather function

--- assets/script.js
+++ assets/script.js
@@ -12,23 +12,31 @@ function searchSubmit(e) {
 // Function to fetch city based on search input and runs the current weather function and 5 day forecast function
 function getCity(city) {
   fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
   )
     .then((res) => res.json())
     .then((city) => {
       currentWeather(city);
-      //   get5DayCast(city.coord.lat, city.coord.lon);
+      get5DayCast(city.coord.lat, city.coord.lon);
     })
     .catch((error) => console.log(error));
 }
 
+//Current Weather function to insert returned API results onto the page
 function currentWeather(city) {
   const cityNameEl = document.getElementById(`selected-city-name`);
+  const tempEl = document.getElementById(`temp-current`);
+  const windEl = document.getElementById(`wind-current`);
+  const humidityEl = document.getElementById(`humidity-current`);
+
   cityNameEl.textContent = `${city.name} (${new Date().toLocaleDateString()})`;
+  tempEl.textContent = `${city.main.temp}°C`;
+  windEl.textContent = `${city.wind.speed} km/h`;
+  humidityEl.textContent = `${city.main.humidity}%`;
 }
 
 function get5DayCast(lat, lon) {
   fetch(
-    `https://api.openweathermap.org/data/2.5/forecast?lat=${cityLat}&lon=${cityLon}&units=metric&appid=${apiKey}`
+    `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`
   );
 }
