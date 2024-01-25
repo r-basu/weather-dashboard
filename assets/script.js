95845c33af9efc26f7689be80cf15c0be3dbd41b
Rahul Basu <142247733+r-basu@users.noreply.github.com>
2024-01-24 19:23:19 -0500

    Add city name and date to currentWeather function

--- assets/script.js
+++ assets/script.js
@@ -2,33 +2,33 @@ const apiKey = `bd13bf9d049abe818932565b25112319`;
 
 //Setting up Search button
 const searchBtn = document.getElementById(`search-btn`);
-const searchInput = document.getElementById(`search-input`).value.trim();
-searchBtn.addEventListener("submit", searchSubmit);
-
+searchBtn.addEventListener(`click`, searchSubmit);
 function searchSubmit(e) {
+  let cityName = document.getElementById(`search-input`).value.trim();
   e.preventDefault();
-  getCity(searchInput);
+  getCity(cityName);
 }
 
 // Function to fetch city based on search input and runs the current weather function and 5 day forecast function
 function getCity(city) {
   fetch(
-    `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${apiKey}`
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
   )
-    .then((res) => res.json)
+    .then((res) => res.json())
     .then((city) => {
       currentWeather(city);
-      get5DayCast(city[0].lat, city[0].lon);
+      //   get5DayCast(city.coord.lat, city.coord.lon);
     })
     .catch((error) => console.log(error));
 }
 
-function currentWeather(city) {}
+function currentWeather(city) {
+  const cityNameEl = document.getElementById(`selected-city-name`);
+  cityNameEl.textContent = `${city.name} (${new Date().toLocaleDateString()})`;
+}
 
 function get5DayCast(lat, lon) {
   fetch(
     `https://api.openweathermap.org/data/2.5/forecast?lat=${cityLat}&lon=${cityLon}&units=metric&appid=${apiKey}`
   );
 }
-
-
